1b5c9242097ba938ebd940953994924a734f61f4
Hieu Doan <hieudoan280102@gmail.com>
2023-06-06 22:20:54 +0700

    b13

--- src/App.js
+++ src/App.js
@@ -1,33 +1,31 @@
 
 import './App.css';
-
+import { YouTubeData } from './YoutubeData';
+console.log(YouTubeData);
 function App() {
   return (
     <div className="youtube-list">
-      <YoutubeItem  image="https://i.pinimg.com/564x/cf/19/a9/cf19a9ffe6c7b98ef0bef5532710ad0a.jpg"
-      avatar="https://i.pinimg.com/564x/d7/27/82/d727820bed8d12f55429743579708619.jpg"
-      author = "Doan Hieu"
-      title = "Hi! I love it!"
+
+      {YouTubeData.map((item, index) => {
+        let newClass ="";
+        if(index === 1) newClass ="abc";
+
+        // const newClass = index === 1 ? "abc" : "";
+        return (
+
+        <YoutubeItem
+        key ={item.id}
+        image={item.image || item.avatar}
+        avatar = {item.avatar || item.image}
+        author = {item.author}
+        title = {item.title}
+        className={newClass}
+        ></YoutubeItem>
+        );
+        })}
+
+
       
-      />
-      <YoutubeItem  image="https://i.pinimg.com/564x/a3/8d/fe/a38dfe252220b74a6a2eddabacaf80e2.jpg"
-      avatar="https://i.pinimg.com/564x/9f/81/f0/9f81f0fb03ac511fc7003420fb0f1d85.jpg"
-      author = "Doan Hieu"
-      title = "Hi! I fall in love!"
-      
-      />
-      <YoutubeItem  image="https://i.pinimg.com/564x/08/62/e0/0862e06f65aec9818f3e96dfeee4a157.jpg"
-      avatar="https://i.pinimg.com/564x/1e/fc/10/1efc10cf5299a2d94f554021c2a45725.jpg"
-      author = "Doan Hieu"
-      title = "Hi! I am so cute!"
-      
-      />
-      <YoutubeItem  image="https://i.pinimg.com/564x/c4/be/33/c4be335fca52ffe2ecd465f3ee02088f.jpg"
-      avatar="https://i.pinimg.com/564x/75/61/36/7561367f3fdbb70de2c208bcbb56da97.jpg"
-      author = "Doan Hieu"
-      title = "Hi! I love sunny!"
-      
-      />
 
     </div>
   );
@@ -39,7 +37,7 @@ function YoutubeItem(props) {
   console.log(props);
   return (
 
-    <div className="youtube-item">
+    <div className={'youtube-item ${props.classname}'}>
 
       <div className="youtube-image">
 
---
+++ src/YoutubeData.js
@@ -0,0 +1,47 @@
+export const YouTubeData = [
+
+    {
+        id: 1,
+        image: "https://i.pinimg.com/564x/cf/19/a9/cf19a9ffe6c7b98ef0bef5532710ad0a.jpg",
+        avatar: "https://i.pinimg.com/564x/d7/27/82/d727820bed8d12f55429743579708619.jpg",
+        author: "Doan Hieu",
+
+        title: "Welcome to learning ReactJS"
+    },
+
+    {
+
+        id: 2,
+        image: "https://i.pinimg.com/564x/a3/8d/fe/a38dfe252220b74a6a2eddabacaf80e2.jpg",
+        avatar: "https://i.pinimg.com/564x/9f/81/f0/9f81f0fb03ac511fc7003420fb0f1d85.jpg",
+        author: "Doan Hieu",
+
+        title: "Hi! I love white flowers!"
+    },
+
+
+
+    {
+        id: 3,
+        image: "https://i.pinimg.com/564x/08/62/e0/0862e06f65aec9818f3e96dfeee4a157.jpg",
+        avatar: "https://i.pinimg.com/564x/1e/fc/10/1efc10cf5299a2d94f554021c2a45725.jpg",
+        author: "Doan Hieu",
+
+        title: "Hi! This is sunny day!"
+    },
+
+    {
+        id: 4,
+        image: "https://i.pinimg.com/564x/c4/be/33/c4be335fca52ffe2ecd465f3ee02088f.jpg",
+        avatar: "https://i.pinimg.com/564x/75/61/36/7561367f3fdbb70de2c208bcbb56da97.jpg",
+        author: "Doan Hieu",
+        title: "Hi! I feel happy!"
+    },
+
+
+
+
+
+
+
+];
